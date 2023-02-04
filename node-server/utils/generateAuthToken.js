e6d2ee048dd7d64c90a54eb6d1454f13f1eae0ff
Jaykishan Kotak <jaykishankotak@gmail.com>
2023-02-04 21:21:51 +0530

    Token Time Increased

--- node-server/utils/generateAuthToken.js
+++ node-server/utils/generateAuthToken.js
@@ -4,7 +4,7 @@ const generateAuthToken = (_id, firstName, lastName, email, isAdmin) => {
   return jwt.sign(
     { _id, firstName, lastName, email, isAdmin },
     process.env.JWT_SECRET_KEY,
-    { expiresIn: "2h" }
+    { expiresIn: "12h" }
   );
 };
 
